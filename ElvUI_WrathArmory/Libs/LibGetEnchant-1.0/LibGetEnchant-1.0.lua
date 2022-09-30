8ed87ce1a25aa870e0e0d4ce13101e8ae4355934
Repooc <tjcooper1982@gmail.com>
2022-09-30 16:42:04 -0400

    update enchant db

--- ElvUI_WrathArmory/Libs/LibGetEnchant-1.0/LibGetEnchant-1.0.lua
+++ ElvUI_WrathArmory/Libs/LibGetEnchant-1.0/LibGetEnchant-1.0.lua
@@ -1,5 +1,5 @@
 local MAJOR_VERSION = "LibGetEnchant-1.0"
-local MINOR_VERSION = 1
+local MINOR_VERSION = 2
 if not LibStub then error(MAJOR_VERSION .. " requires LibStub.") end
 local lib = LibStub:NewLibrary(MAJOR_VERSION, MINOR_VERSION)
 if not lib then return end
