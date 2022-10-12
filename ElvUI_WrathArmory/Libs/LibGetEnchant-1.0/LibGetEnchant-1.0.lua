5fb5d4384b3dfe3da4490c6e144d6b8d0fed1b74
Repooc <tjcooper1982@gmail.com>
2022-10-12 04:09:18 -0400

    ocd issues

--- ElvUI_WrathArmory/Libs/LibGetEnchant-1.0/LibGetEnchant-1.0.lua
+++ ElvUI_WrathArmory/Libs/LibGetEnchant-1.0/LibGetEnchant-1.0.lua
@@ -1,7 +1,7 @@
-local MAJOR_VERSION = "LibGetEnchant-1.0-WrathArmory"
-local MINOR_VERSION = 3
-if not LibStub then error(MAJOR_VERSION .. " requires LibStub.") end
-local lib = LibStub:NewLibrary(MAJOR_VERSION, MINOR_VERSION)
+local _, ns = ...
+local MAJOR, MINOR = "LibGetEnchant-1.0-WrathArmory", 3
+assert(LibStub, MAJOR.." requires LibStub")
+local lib = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end
 
 lib.callbacks = lib.callbacks or LibStub("CallbackHandler-1.0"):New(lib)
